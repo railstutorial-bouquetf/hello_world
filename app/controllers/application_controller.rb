92ac4cd672e75c6e1d570588d7fca7ab6d057608
Frédéric Bouquet <bouquet.frederic@gmail.com>
2018-10-05 22:39:06 +0200

    1.5.3.1 Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
+  def hola
     render html: "¡Hola, mundo!"
   end
 
   def goodbye
     render html: "goodbye, world!"
+  end
+
+  def hello
+    render html: "hello, world!"
   end
 end
